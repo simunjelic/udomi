e6a9f8b953a34b5546a2bc0bea4c360fc76a40e9
simunjelic <simun.jelic@gmail.com>
2021-12-11 14:29:11 +0100

    laravel

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function pocetna()
+    {
+        return view('pocetna');
+    }
+    public function onama()
+    {
+        return view('onama');
+    }
+}
